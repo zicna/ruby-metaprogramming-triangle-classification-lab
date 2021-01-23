9f7f636e98c2a7e867825a3e054048ab18cc7a23
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-23 16:35:12 -0600

    commit tree b9cb24b131bbeea2be802d3c2983cf6a2f2d30e0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    if  a + b <= c || a + c <= b || c + b <= a
+      raise TriangleError
+    elsif a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif  a == nil || b == nil  || c == nil
+      raise TriangleError
+    end
+
+    @a = a
+    @b = b
+    @c = c
+
+  end
+
+  def kind
+    if self.a == self.b && self.b == self.c
+      return :equilateral
+    elsif self.a == self.b || self.a == self.c || self.b == self.c
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    # def message
+    #   puts "This is not valid triangle!"
+    # end
+  end
+
 end
+
+
+# if a + b < + c
+    #   raise TriangleError
+    # end
+
+    # if a <= 0 || b <= 0 || c <= 0
+    #   raise TriangleError
+    # end
+    
+    # if a == nil || b == nil || c == nil
+    #   raise TriangleError
+    # end
